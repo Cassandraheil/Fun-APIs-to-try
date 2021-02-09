ccadae78d20da1e343e25439f9a093ae63fa569a
Cassandra <cassandraheil19@gmail>
2021-02-09 16:14:14 -0500

    kanye quote section call completes and shows

--- assets/APIs/KanyeQuote.js
+++ assets/APIs/KanyeQuote.js
@@ -1,8 +1,25 @@
+var div = $("<div id='kanyeDiv'>");
+$("section").append(div)
 
 
-$.ajax({
-    url: "https://api.kanye.rest",
-    method: "GET"
-}).then(function(response){
-    console.log(response)
+
+var button = document.createElement("button");
+button.innerHTML = "click for Kanye's words of wisdom"
+button.classList.add("kanyeBtn")
+$("#kanyeDiv").append(button)
+
+
+
+
+$(".kanyeBtn").click(function(){
+    
+    $.ajax({
+        url: "https://api.kanye.rest",
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+        var kanyeQuote = response
+
+        $("#kanyeDiv").prepend(response.quote)
+    })
 })
