10d063f5a9563159ef8ab58481fc9a4312fabc75
Cassandra <cassandraheil19@gmail>
2021-02-19 11:46:34 -0500

    catfacts api added and working

---
+++ assets/APIs/catFacts.js
@@ -0,0 +1,32 @@
+var div = $("<div id='catDiv'>");
+$("section").append(div)
+
+
+
+var button = document.createElement("button");
+button.innerHTML = "Cat Facts!"
+button.classList.add("catFactBtn")
+$("#catDiv").append(button)
+
+
+
+
+$.ajax({
+    url: "https://cat-fact.herokuapp.com/facts",
+    method: "GET"
+}).then(function(response){
+
+    catFactsArr = []
+
+    for (i = 0; i < response.length; i++) {
+        
+        var catFacts = response[i].text
+        console.log("catfacts response",response[i].text)
+        catFactsArr.push(catFacts)
+    }
+    console.log("le arrray",catFactsArr)
+
+
+
+    // $("#catDiv").prepend(response + "<br>")
+})
