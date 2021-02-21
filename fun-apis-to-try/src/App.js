ef14f2328d2d4b56ccf93e732f56a2e85bc1109d
Cassandra <cassandraheil19@gmail>
2021-02-21 17:18:26 -0500

    added basic components and changed picture

--- fun-apis-to-try/src/App.js
+++ fun-apis-to-try/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://expertphotography.com/wp-content/uploads/2011/06/how-to-take-good-pictures-waterlilly.jpg" className="App-logo" alt="logo" />
+        <img src="https://expertphotography.com/wp-content/uploads/2011/06/how-to-take-good-pictures-waterlilly.jpg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ehello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ fun-apis-to-try/src/components/card/card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./card.css";
+
+function apiCards(){
+    return(
+        <div className='apiCard'>
+        <p>"here is a card"</p>
+        </div>
+    )
+}
