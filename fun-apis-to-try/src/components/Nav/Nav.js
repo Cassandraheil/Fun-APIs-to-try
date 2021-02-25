f31d9f53811b52d3a54944cf6e8b05534570af79
Cassandra <cassandraheil19@gmail>
2021-02-24 20:41:56 -0500

    nav bar lookin good

--- fun-apis-to-try/src/components/Nav/Nav.js
+++ fun-apis-to-try/src/components/Nav/Nav.js
@@ -7,8 +7,8 @@ function Nav(){
         <nav className="navbar navbar-light bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="#" >
-              <img src="https://academy.mobidea.com/wp-content/uploads/2017/12/api.jpg" alt="" width="30" height="24" class="d-inline-block align-top"></img>
-              <p style={{color:'white'}}>API's are the Bomb.com!</p>
+              <img src="https://academy.mobidea.com/wp-content/uploads/2017/12/api.jpg" alt="api-pic" width="30" height="24" className="d-inline-block align-top"></img>
+              <p className="navText">API's are the Bomb.com!</p>
             </a>
           </div>
         </nav> 
