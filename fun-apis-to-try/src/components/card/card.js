8d9ffb65d12aa211a81cb374ea7c26838e9bdf6a
Cassandra <cassandraheil19@gmail>
2021-02-24 20:53:44 -0500

    basic blueprint info for the cards

--- fun-apis-to-try/src/components/card/card.js
+++ fun-apis-to-try/src/components/card/card.js
@@ -4,7 +4,13 @@ import "./card.css";
 function ApiCards(){
     return(
         <div className='apiCard'>
-        <p>here is a card that will look super nice and professional </p>
+            <div>
+            <p>this shall be information from the api's</p>
+            {/* class="btn btn-{props}">{props} */}
+            <button type="button" class="btn btn-info">Click for Kayne's words of wisdom</button>
+            </div>
+            {/* the src will be props soon enough */}
+            <img src="https://www.protexinvet.com/userfiles/image/cute-2500929_1920_(1).jpg"></img>
         </div>
     )
 }
